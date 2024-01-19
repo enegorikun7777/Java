2ecb75c67bd03d8876d32dabf9b274ea03d8b29f
enegorikun7777 <ktq2390323@stu.o-hara.ac.jp>
2024-01-19 14:10:53 +0900

    Commit

--- test/program11/Circle.java
+++ test/program11/Circle.java
@@ -1,10 +1,11 @@
-package test.program11.Mondai1;
+package test.program11;
 
 import java.util.Scanner;
-class Mondai1 {
+
+public class Circle { 
    private static final double PI = 3.1415;
    private double radius;
-   public Mondai1(double radius) {
+   public Circle(double radius) {
        this.radius = radius;
    }
    //円周の長さ
@@ -21,11 +22,11 @@ class CircleProgram {
        Scanner scanner = new Scanner(System.in);
        System.out.print("半径を整数値で入力：");
        double radius = scanner.nextDouble();
-       Mondai1 circle = new Mondai1(radius);
+       Circle circle = new Circle(radius);
        
        System.out.println("円周の長さは" + circle.calculateCircumference() + "です。");
        System.out.printf("円の面積は%.3fです。", circle.calculateArea());
        scanner.close();
    }
 }
-    
+
