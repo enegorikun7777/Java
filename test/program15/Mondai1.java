82dbf084c76c27457fb4c9a69a1ef42ce709f98c
enegorikun7777 <ktq2390323@stu.o-hara.ac.jp>
2024-01-29 10:07:19 +0900

    Commit

--- test/program15/Mondai1.java
+++ test/program15/Mondai1.java
@@ -6,25 +6,25 @@ public class Mondai1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try {
-        // 1つ目の整数入力
-        System.out.print("整数1 = ");
-        int num1 = scanner.nextInt();
+            // 1つ目の整数入力
+            System.out.print("整数1 = ");
+            int num1 = scanner.nextInt();
 
-        // 2つ目の整数入力
-        System.out.print("整数2 = ");
-        int num2 = scanner.nextInt();
+            // 2つ目の整数入力
+            System.out.print("整数2 = ");
+            int num2 = scanner.nextInt();
 
-        // num2がゼロでないか確認して、ゼロでの除算を避ける
-        if (num2 == 0) {
-            // もしnum2がゼロなら、ArithmeticExceptionをスローする
-            throw new ArithmeticException();
-        }
+            // num2がゼロでないか確認して、ゼロでの除算を避ける
+            // if (num2 == 0) {
+            //     // もしnum2がゼロなら、ArithmeticExceptionをスローする
+            //     throw new ArithmeticException();
+            // }
 
-        // 割り算の実行
-        double result = (double)num1 / num2;
+            // 割り算の実行
+            double result = num1 / num2;
 
-        // 結果の表示
-        System.out.println(num1 + " / " + num2 + " = " + result);
+            // 結果の表示
+            System.out.println(num1 + " / " + num2 + " = " + result);
         } catch (ArithmeticException e) {
             // 0で割った場合の例外処理
             System.out.println("0による割り算です!!");
